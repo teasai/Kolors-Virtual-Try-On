68b15ecbac64cd4ab35fa2beff7dd95c8f3b4ee2
lixiang46 <lixiang46@kuaishou.com>
2024-08-12 15:40:35 +0800

    add examples

--- app.py
+++ app.py
@@ -142,7 +142,8 @@ with gr.Blocks(css=css) as Tryon:
         show_case = gr.Examples(
             examples=[
                 ["assets/examples/model1.png", "assets/examples/garment1.png", "assets/examples/result1.png"],
-                ["assets/examples/model2.png", "assets/examples/garment2.png", "assets/examples/result2.png"]
+                ["assets/examples/model2.png", "assets/examples/garment2.png", "assets/examples/result2.png"],
+                ["assets/examples/model3.png", "assets/examples/garment3.png", "assets/examples/result3.png"],
             ],
             inputs=[image1, image2, image3],
             label=None
