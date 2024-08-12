d77e6639a1326c93dbea0008fdefb58e9feaf222
lixiang46 <lixiang46@kuaishou.com>
2024-08-12 11:51:41 +0800

    test

--- app.py
+++ app.py
@@ -141,14 +141,10 @@ with gr.Blocks(css=css) as Tryon:
             ],
             inputs=[image1, image2, image3],
             label=None,
+            fn = start_tryon(image1, image2, seed, randomize_seed),
+            outputs = [image_out, seed_used, result_info]
         )
-    
-    # gr.on(
-    #     triggers = [show_case.change],
-    #     fn=start_tryon, 
-    #     inputs=[image1, image2, seed, randomize_seed], 
-    #     outputs=[image_out, seed_used, result_info]
-    # )
+
 
 ip = requests.get('http://ifconfig.me/ip', timeout=1).text.strip()
 print("ip address", ip)
