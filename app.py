a785755b479c74df75c7d3b7952fb3ff3210a889
lixiang46 <lixiang46@kuaishou.com>
2024-08-15 10:55:02 +0800

    change timeout

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def start_tryon(person_img, garment_img, seed, randomize_seed):
 
     result_img = None
     try:
-        response = requests.post(url, headers=headers, data=json.dumps(data), timeout=60)
+        response = requests.post(url, headers=headers, data=json.dumps(data), timeout=120)
         print("response code", response.status_code)
         if response.status_code == 200:
             result = response.json()['result']
