18f423a6393b5cf2623c041acebe67d214224d60
lixiang46 <lixiang46@kuaishou.com>
2024-08-15 18:21:23 +0800

    add test button

--- app.py
+++ app.py
@@ -8,6 +8,68 @@ import base64
 import requests
 import json
 import time
+from requests.adapters import HTTPAdapter
+
+def tryon(person_img, garment_img, seed, randomize_seed):
+    start_time = time.time()
+    if person_img is None or garment_img is None:
+        return None, None, "Empty image"
+    if randomize_seed:
+        seed = random.randint(0, MAX_SEED)
+    encoded_person_img = cv2.imencode('.jpg', cv2.cvtColor(person_img, cv2.COLOR_RGB2BGR))[1].tobytes()
+    encoded_person_img = base64.b64encode(encoded_person_img).decode('utf-8')
+    encoded_garment_img = cv2.imencode('.jpg', cv2.cvtColor(garment_img, cv2.COLOR_RGB2BGR))[1].tobytes()
+    encoded_garment_img = base64.b64encode(encoded_garment_img).decode('utf-8')
+
+    url = "http://" + os.environ['tryon_url'] + "Submit"
+    token = os.environ['token']
+    cookie = os.environ['Cookie']
+    referer = os.environ['referer']
+    headers = {'Content-Type': 'application/json', 'token': token, 'Cookie': cookie, 'referer': referer}
+    data = {
+        "clothImage": encoded_garment_img,
+        "humanImage": encoded_person_img,
+        "seed": seed
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data), timeout=40)
+    print("post code", response.status_code)
+    if response.status_code == 200:
+        result = response.json()['result']
+        status = result['status']
+        if status == "success":
+            uuid = result['uuid']
+            print(uuid)
+
+    try:
+        url = "http://" + os.environ['tryon_url'] + "Query?" + uuid
+        session = requests.Session()
+        session.mount("http://",  HTTPAdapter(max_retries=3))
+        response = session.get(url, headers=headers, timeout=10)
+        print("response code", response.status_code)
+        if response.status_code == 200:
+            result = response.json()['result']
+            status = result['status']
+            if status == "success":
+                result = base64.b64decode(result['result'])
+                result_np = np.frombuffer(result, np.uint8)
+                result_img = cv2.imdecode(result_np, cv2.IMREAD_UNCHANGED)
+                result_img = cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR)
+                info = "Success"
+            else:
+                info = "Try again latter"
+        else:
+            print(response.text)
+            info = "URL error, pleace contact the admin"
+    except requests.exceptions.ReadTimeout:
+        print("timeout")
+        info = "Too many users, please try again later"
+        raise gr.Error("Too many users, please try again later")
+    end_time = time.time()
+    print(f"time used: {end_time-start_time}")
+
+    return result_img, seed, info
+    
+
 
 
 def start_tryon(person_img, garment_img, seed, randomize_seed):
@@ -162,9 +224,11 @@ with gr.Blocks(css=css) as Tryon:
                 seed_used = gr.Number(label="Seed used")
                 result_info = gr.Text(label="Response")
             try_button = gr.Button(value="Run", elem_id="button")
+            test_button = gr.Button(value="Text", elem_id="button")
 
 
     try_button.click(fn=start_tryon, inputs=[imgs, garm_img, seed, randomize_seed], outputs=[image_out, seed_used, result_info], api_name='tryon',concurrency_limit=10)
+    test_button.click(fn=tryon, inputs=[imgs, garm_img, seed, randomize_seed], outputs=[image_out, seed_used, result_info], api_name='tryon',concurrency_limit=10)
 
     with gr.Column(elem_id = "col-showcase"):
         gr.HTML("""
